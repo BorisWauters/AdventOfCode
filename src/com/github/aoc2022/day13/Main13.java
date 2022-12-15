85389ca41642abdbcf8c528f08f3eadf30cd5044
Boris Wauters <boris.wauters@iqgeo.com>
2022-12-15 12:37:02 +0100

    AOC 2022 day 13

---
+++ src/com/github/aoc2022/day13/Main13.java
@@ -0,0 +1,56 @@
+package com.github.aoc2022.day13;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Main13 {
+
+    public static void main(String[] args) {
+        try (Stream<String> stream = Files.lines(Paths.get("input/aoc2022/day13.txt"))) {
+            solve(stream.toArray(String[]::new));
+        } catch (IOException e) {
+            System.err.println(e.getLocalizedMessage());
+        }
+    }
+
+    private static void solve(String[] lines) {
+        List<Packet> packets = new ArrayList<>();
+
+        for (String line : lines) {
+            List<Packet> p = new ArrayList<>();
+            Packet.build(line, p);
+            packets.addAll(p);
+        }
+        // Part 1
+        int sum = 0;
+        for (int i = 0; i < packets.size() / 2; i++) {
+            Packet p1 = packets.get(2 * i);
+            Packet p2 = packets.get(2 * i + 1);
+
+            int index = i + 1;
+            if (p1.compareTo(p2) <= 0) {
+                sum += index;
+            }
+        }
+
+        System.out.println("Part 1: " + sum);
+
+        // Part 2
+        List<Packet> dividers = new ArrayList<>(2);
+        Packet.build("[[2]]", dividers);
+        Packet.build("[[6]]", dividers);
+        packets.addAll(dividers);
+
+        packets.sort((p1, p2) -> p1.compareTo(p2));
+
+        int decoderKey = 1;
+        for (Packet divider : dividers) {
+            decoderKey *= (packets.indexOf(divider) + 1);
+        }
+        System.out.println("Part 2: " + decoderKey);
+    }
+}
---
+++ src/com/github/aoc2022/day13/Packet.java
@@ -0,0 +1,90 @@
+package com.github.aoc2022.day13;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Packet implements Comparable<Packet> {
+    private int value;
+    private List<Packet> packets;
+    private final boolean integer;
+
+    public Packet(int value) {
+        this.value = value;
+        this.integer = true;
+    }
+
+    public Packet(List<Packet> packets) {
+        this.packets = packets;
+        this.integer = false;
+    }
+
+    @Override
+    public int compareTo(Packet packet) {
+        if (integer && packet.integer) {
+            return Integer.compare(value, packet.value);
+        }
+
+        if (integer) {
+            packets = List.of(new Packet(value));
+        }
+
+        if (packet.integer) {
+            packet.packets = List.of(new Packet(packet.value));
+        }
+
+        if (packets.isEmpty()) {
+            return packet.packets.isEmpty() ? 0 : -1;
+        }
+
+        for (int i = 0; i < packets.size(); i++) {
+            if (i >= packet.packets.size())
+                return 1;
+
+            int comp = packets.get(i).compareTo(packet.packets.get(i));
+            if (comp != 0)
+                return comp;
+        }
+
+        return packets.size() < packet.packets.size() ? -1 : 0;
+    }
+
+    @Override
+    public String toString() {
+        if (integer) {
+            return String.valueOf(value);
+        }
+
+        StringBuilder stb = new StringBuilder();
+        stb.append("[");
+        for (int i = 0; i < packets.size(); i++) {
+            stb.append(packets.get(i)).append(",");
+        }
+        stb.append("]");
+        return stb.toString().replace(",]", "]");
+    }
+
+    public static int build(String line, List<Packet> packets) {
+        int last = 0;
+        for (int i = 0; i < line.length(); i++) {
+            if (last != i && (line.charAt(i) == ',' || line.charAt(i) == ']')) {
+                packets.add(new Packet(Integer.parseInt(line.substring(last, i))));
+            }
+
+            if (line.charAt(i) == ',') {
+                last = i + 1;
+            }
+
+            if (line.charAt(i) == ']') {
+                return i;
+            }
+
+            if (line.charAt(i) == '[') {
+                List<Packet> p = new ArrayList<>();
+                i += build(line.substring(i + 1), p) + 1;
+                last = i + 1;
+                packets.add(new Packet(p));
+            }
+        }
+        return 0;
+    }
+}
