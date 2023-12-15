78f279d3b1b46990ad297a7de398fee40d62ead0
Wauters B. <boris.wauters@iqgeo.com>
2023-12-15 23:28:02 +0100

    AOC 2023 day 11

---
+++ src/com/github/aoc2023/day11/Galaxy.java
@@ -0,0 +1,39 @@
+package com.github.aoc2023.day11;
+
+import java.util.Set;
+
+public class Galaxy {
+    private int row;
+    private int col;
+    private final int offset;
+
+    public Galaxy(int row, int col, int offset) {
+        this.row = row;
+        this.col = col;
+        this.offset = offset;
+    }
+
+    public void correction(Set<Integer> expandedRows, Set<Integer> expandedCols) {
+        int newRow = this.row;
+        int newCol = this.col;
+
+        for (int i = 0; i < this.row; i++) {
+            if (expandedRows.contains(i)) {
+                newRow += this.offset - 1;
+            }
+        }
+
+        for (int i = 0; i < this.col; i++) {
+            if (expandedCols.contains(i)) {
+                newCol += this.offset - 1;
+            }
+        }
+
+        this.row = newRow;
+        this.col = newCol;
+    }
+
+    public int getDistance(Galaxy other) {
+        return Math.abs(this.row - other.row) + Math.abs(this.col - other.col);
+    }
+}
---
+++ src/com/github/aoc2023/day11/Main11.java
@@ -0,0 +1,57 @@
+package com.github.aoc2023.day11;
+
+import com.github.Solution;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main11 extends Solution {
+    @Override
+    protected void solve(String[] lines) {
+        System.out.println("Part 1: " + getDistance(lines, 2));
+        System.out.println("Part 2: " + getDistance(lines, 1000000));
+    }
+
+    private long getDistance(String[] lines, int offset) {
+        Set<Integer> expandedRows = new HashSet<>();
+        Set<Integer> expandedCols = new HashSet<>();
+        for (int i = 0; i < lines.length; i++) {
+            expandedRows.add(i);
+        }
+        for (int i = 0; i < lines[0].length(); i++) {
+            expandedCols.add(i);
+        }
+
+        Set<Galaxy> galaxies = new HashSet<>();
+
+        for (int i = 0; i < lines.length; i++) {
+            String line = lines[i];
+            for (int j = 0; j < line.length(); j++) {
+                char c = line.charAt(j);
+
+                if (c == '#') {
+                    Galaxy galaxy = new Galaxy(i, j, offset);
+                    galaxies.add(galaxy);
+
+                    expandedRows.remove(i);
+                    expandedCols.remove(j);
+                }
+            }
+        }
+
+        for (Galaxy galaxy : galaxies) {
+            galaxy.correction(expandedRows, expandedCols);
+        }
+
+
+        long sum = 0;
+        Set<Galaxy> remainingGalaxies = new HashSet<>(galaxies);
+        for (Galaxy galaxy : galaxies) {
+            remainingGalaxies.remove(galaxy);
+            sum += remainingGalaxies.stream()
+                    .mapToLong(galaxy::getDistance)
+                    .sum();
+        }
+        return sum;
+    }
+}
